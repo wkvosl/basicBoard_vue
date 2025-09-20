1020f5f2e99150972c7af4713af79d36decaa6ee
madsun <madsun@kakao.com>
2025-09-20 17:45:11 +0900

    레이아웃 빠뜨린거

---
+++ src/components/layout/ContentLayout.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import Footer from "@/components/layout/Footer.vue";
+import Nav from "@/components/layout/Nav.vue";
+</script>
+
+<template>
+  <div class="main-layout">
+    <header class="header">
+
+      <Nav />
+
+    </header>
+
+    <main class="content">
+        <div class="flex justify-center">
+          <div class="w-4/5 min-w-fit">
+            <router-view :key="$route.fullPath"></router-view>
+          </div>
+        </div>
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+.main-layout {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+}
+</style>
